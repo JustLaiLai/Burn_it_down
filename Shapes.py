6e98a504a77655baf34a3891da2d123b78fe1302
JustLailai <dianamilasan@gmail.com>
2019-03-09 20:36:57 +0200

    Added unit test

--- Shapes.py
+++ Shapes.py
@@ -1,11 +1,11 @@
 from abc import ABC, abstractmethod
 import math
+import unittest
 
 class Shapes(ABC):
     def __init__(self, sides, size):
         self.size = size
         self.sides = sides
-        print(self.sides)
         super().__init__()
 
     def parameter(self):
@@ -23,8 +23,21 @@ class Triangle(Shapes):
     def area(self):
         return pow(self.size,2) * math.sqrt(3) / 4
 
+class TestArea(unittest.TestCase):
+
+    def test_result(self):
+        self.assertEqual(x.area(),100)
+
+    def test_result_type(self):
+        self.assertTrue(type(y.area())==float)
+        self.assertFalse(type(y.area())==int)
+
+
 x = Square(4,10)
 print('Square area ',x.area())
 print('Square parameter ',x.parameter())
 y = Triangle(3,3)
 print('Triangle area ',y.area(),' and its parameter',x.parameter())
+
+if __name__=='__main__':
+    unittest.main()
